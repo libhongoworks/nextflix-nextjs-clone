47ab5bebf81512173ad06b28caba1a1f8940b813
Libhongo Langa <lnglib001@myuct.ac.za>
2018-12-15 07:51:13 +0200

    minor fixes to landing page elements

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cd3ada9e11b496ae2a8d";
+/******/ 	var hotCurrentHash = "a4d132c75606ea0aa9da";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
